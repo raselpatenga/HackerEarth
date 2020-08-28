be20aa13373f6e5f472a08be5e5fd768c0b4b3f4
anowereng <anowereng@gmail.com>
2020-08-28 12:21:08 +0600

    Doctors_Secret==Done

---
+++ HackerEarth_VeryEasy/Doctors_Secret/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doctors_Secret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] inputs = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            if (inputs[0] <= 23 && inputs[1] >= 500 && inputs[1] <= 1000)
+            {
+                Console.WriteLine("Take Medicine");
+            }
+            else
+            {
+                Console.WriteLine("Don't take Medicine");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
