1bcb90044bb806fcd4123aaf8641ff818145f257
anowereng <anowereng@gmail.com>
2020-08-28 12:23:57 +0600

    I_am_easy==Done

---
+++ HackerEarth_VeryEasy/I_am_easy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_am_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(A * i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
