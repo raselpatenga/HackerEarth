b1dc8756be51f737300ec0891d1755fd00da319b
anowereng <anowereng@gmail.com>
2020-08-28 12:13:01 +0600

    Teddy_and_Tweety==Done

---
+++ HackerEarth_VeryEasy/Teddy_and_Tweety/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teddy_and_Tweety
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Chocolet = Convert.ToInt32(Console.ReadLine());
+
+            if (Chocolet % 3 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadKey();
+        }
+    }
+}
