6f0a6e8867d7702540c1bd4035034841ae52d07c
RASEL <raselpatenga@gmail.com>
2020-08-31 14:47:59 +0600

    Roy_and_Profile_Picture == Done

---
+++ HackerEarth_VeryEasy/Roy_and_Profile_Picture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roy_and_Profile_Picture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Length of picture
+            int L = Convert.ToInt32(Console.ReadLine());
+            //Number of picture
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                //width and hight of picture
+                string[] WH = Console.ReadLine().Split(' ');
+                int W = Convert.ToInt32(WH[0]);
+                int H = Convert.ToInt32(WH[1]);
+
+                if (W < L || H < L)
+                {
+                    Console.WriteLine("UPLOAD ANOTHER");
+                }
+                else if (W == H && W == L)
+                {
+                    Console.WriteLine("ACCEPTED");
+                }
+                else if (W == H && (W > L || H > L))
+                {
+                    Console.WriteLine("ACCEPTED");
+                }
+                else if (W > L || H > L)
+                {
+                    Console.WriteLine("CROP IT");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
