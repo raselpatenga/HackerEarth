a0e33daf84541947d6f1ae15bc2b946bd32169ef
RASEL <raselpatenga@gmail.com>
2020-08-31 15:45:04 +0600

    Goki_and_his_breakup == Done

---
+++ HackerEarth_VeryEasy/Goki_and_his_breakup/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goki_and_his_breakup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfFriends = Convert.ToInt32(Console.ReadLine());
+            int minimumskills = Convert.ToInt32(Console.ReadLine());
+            int[] friends = new int[numberOfFriends];
+
+            for(int i = 0; i<numberOfFriends; i++)
+            {
+                friends[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i=0; i<numberOfFriends; i++)
+            {
+                if(friends[i] >= minimumskills)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
