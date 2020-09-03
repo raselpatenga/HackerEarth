b4a7d5009eadc246a9fcd0346b212a73f75610ce
RASEL <raselpatenga@gmail.com>
2020-09-03 13:22:10 +0600

    A.Movement == Done

---
+++ HackerEarth_VeryEasy/A.Movement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.Movement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Position of his Friends
+            int position = Convert.ToInt32(Console.ReadLine());
+            int Result1 = position / 5;
+            int Result2 = 0;
+            
+            if((position % 5 < 5) && (position % 5 != 0))
+            {
+                Result2 = 1;
+            }
+            Console.WriteLine(Result1 + Result2);
+            Console.ReadKey();
+        }
+    }
+}
