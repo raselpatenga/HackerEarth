5faa71d7e92391eaa3bd1d56a91363c46bf1b731
RASEL <raselpatenga@gmail.com>
2020-09-03 17:26:28 +0600

    Char_Sum

---
+++ HackerEarth_VeryEasy/Char_Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Char_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string C = Console.ReadLine();
+            char[] charArry = C.ToCharArray();
+            int Z = 0;
+            
+            for(int i = 0; i < C.Length; i++)
+            {
+                Z =  Z + ((int)C[i] - 96);
+            }
+            Console.WriteLine(Z);
+            Console.ReadKey();
+
+        }
+    }
+}
