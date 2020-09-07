fdbd3f518814450fa9ab73a306d7dcaa6e59a4f8
RASEL <raselpatenga@gmail.com>
2020-09-07 17:57:23 +0600

    e_maze_in == Done

---
+++ HackerEarth_VeryEasy/e_maze_in/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_maze_in
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            string command = Console.ReadLine();
+
+            foreach(char item in command)
+            {
+                switch (item)
+                {
+                    case 'L':
+                        x--;
+                        break;
+                    case 'R':
+                        x++;
+                        break;
+                    case 'D':
+                        y--;
+                        break;
+                    case 'U':
+                        y++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(x + " " + y);
+            Console.ReadKey();
+        }
+    }
+}
