1c54a397640974f2cff700440a66a0d16ae1f660
RASEL <raselpatenga@gmail.com>
2020-09-09 12:22:11 +0600

    Ali_and_Helping_innocent_people == Done

--- HackerEarth_VeryEasy/Ali_and_Helping_innocent_people/Program.cs
+++ HackerEarth_VeryEasy/Ali_and_Helping_innocent_people/Program.cs
@@ -11,25 +11,30 @@ namespace Ali_and_Helping_innocent_people
         static void Main(string[] args)
         {
             string inputs = Console.ReadLine();
+            var flag = false;
 
-            if ((inputs[0] + inputs[1]) % 2 == 0)
+            if (inputs.Length == 9)
             {
-                if (inputs[2] != 'A' && inputs[2] != 'E' && inputs[2] != 'I' && inputs[2] != 'O' && inputs[2] != 'U' && inputs[2] != 'Y')
+                if ((inputs[0] + inputs[1]) % 2 == 0)
                 {
-                    if ((inputs[3] + inputs[4]) % 2 == 0)
+                    if (inputs[2] != 'A' && inputs[2] != 'E' && inputs[2] != 'I' && inputs[2] != 'O' && inputs[2] != 'U' && inputs[2] != 'Y')
                     {
-                        if ((inputs[4] + inputs[5]) % 2 == 0)
+                        if ((inputs[3] + inputs[4]) % 2 == 0)
                         {
-                            if ((inputs[7] + inputs[8]) % 2 == 0)
+                            if ((inputs[4] + inputs[5]) % 2 == 0)
                             {
-                                Console.WriteLine("valid");
+                                if ((inputs[7] + inputs[8]) % 2 == 0)
+                                {
+                                    flag = true;
+                                    Console.WriteLine("valid");
+                                }
                             }
                         }
-                    }
 
+                    }
                 }
             }
-            else
+            if(flag == false)
             {
                 Console.WriteLine("invalid");
             }
