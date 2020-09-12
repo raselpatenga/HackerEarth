59dacd2783993c4fb2bf94f231ddd81b983d924e
RASEL <raselpatenga@gmail.com>
2020-09-12 18:00:16 +0600

    Factorial! == Done

---
+++ HackerEarth_VeryEasy/Factorial!/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            int x = input, result = input;
+
+            for(int i = 1; i < input; i++)
+            {
+                x--;
+                result *= x;
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
