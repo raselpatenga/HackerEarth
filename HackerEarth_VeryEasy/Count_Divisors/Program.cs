f9a4e9e8705c3e533d5469df8d475f1f5f1cf675
RASEL <raselpatenga@gmail.com>
2020-09-14 10:16:40 +0600

    Count_Divisors == Done

---
+++ HackerEarth_VeryEasy/Count_Divisors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Divisors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int l = Convert.ToInt32(input[0]);
+            int r = Convert.ToInt32(input[1]);
+            int k = Convert.ToInt32(input[2]);
+            int count = 0;
+
+            for(int i = l; i <= r; i++)
+            {
+                if (i % k == 0)
+                    count++;
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
