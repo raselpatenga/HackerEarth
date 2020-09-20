52b39b3066d1f3dff94067c1f8f85b45cdc87d46
RASEL <raselpatenga@gmail.com>
2020-09-20 17:42:16 +0600

    Conject_It == Done

---
+++ HackerEarth_VeryEasy/Conject_It/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conject_It
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            for(int i = 0; i < input; i++)
+            {
+                Console.WriteLine("YES");
+            }
+            Console.ReadKey();
+        }
+    }
+}
