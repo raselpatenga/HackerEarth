ce1cead041e5024b5d6abbec05e5474b8e5096ae
RASEL <raselpatenga@gmail.com>
2020-09-21 17:42:42 +0600

    Back_To_School == Done

---
+++ HackerEarth_VeryEasy/Back_To_School/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_To_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int data = input.Max();
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+    }
+}
