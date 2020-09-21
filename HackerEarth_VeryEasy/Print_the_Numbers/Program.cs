ee98e786c8ab187733d00c9119771c52cb9ad3dd
RASEL <raselpatenga@gmail.com>
2020-09-21 17:43:49 +0600

    Print_the_Numbers == Done

---
+++ HackerEarth_VeryEasy/Print_the_Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_the_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int data = Convert.ToInt32(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
