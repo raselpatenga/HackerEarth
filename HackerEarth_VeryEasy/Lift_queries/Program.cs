59648e5c5e520c1ae77684ca9449e97d0973b93b
RASEL <raselpatenga@gmail.com>
2020-09-23 15:02:25 +0600

    Lift_queries == Done

---
+++ HackerEarth_VeryEasy/Lift_queries/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lift_queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, data, leftA = 0, leftB = 7, a, b;
+            input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < input; i++)
+            {
+                data = Convert.ToInt32(Console.ReadLine());
+                a = System.Math.Abs(leftA - data);
+                b = System.Math.Abs(leftB - data);
+
+                if (a <= b)
+                {
+                    Console.WriteLine("A");
+                    leftA = data;
+                }
+                else
+                {
+                    Console.WriteLine("B");
+                    leftB = data;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
