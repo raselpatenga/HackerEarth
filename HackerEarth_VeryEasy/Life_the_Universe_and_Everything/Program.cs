d245e7484b2b423d4fa0e4ec6084f76c2a5ed515
RASEL <raselpatenga@gmail.com>
2020-09-27 17:29:14 +0600

    Life_the_Universe_and_Everything == Done

---
+++ HackerEarth_VeryEasy/Life_the_Universe_and_Everything/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life_the_Universe_and_Everything
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            while(input != 42)
+            {
+                Console.WriteLine(input);
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
