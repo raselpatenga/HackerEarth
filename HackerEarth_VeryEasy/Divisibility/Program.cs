127c60674ceb55b87e7b3f330a812b6757353628
RASEL <raselpatenga@gmail.com>
2020-09-29 17:35:26 +0600

    Divisibility == Done

---
+++ HackerEarth_VeryEasy/Divisibility/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            int isDivisible = intArray[n - 1] % 10;
+            if (isDivisible == 0)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+
+            Console.ReadKey();
+        }
+    }
+}
--- HackerEarth_VeryEasy/Test/Program.cs
+++ HackerEarth_VeryEasy/Test/Program.cs
@@ -10,39 +10,12 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            int testCase = int.Parse(Console.ReadLine());
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < testCase; i++)
-            {
-                string[] colors = Console.ReadLine().Split(' ');
-                int colorOne = int.Parse(colors[0]);
-                int colorTwo = int.Parse(colors[1]);
+            int n = int.Parse(Console.ReadLine());
+            string[] strArray = Console.ReadLine().Split(' ');
+            int[] intArray = Array.ConvertAll(strArray, int.Parse);
 
-                int participant = int.Parse(Console.ReadLine());
-                int one = 0;
-                int two = 0;
-
-                for(int j = 0; j < participant; j++)
-                {
-                    string[] solvedBy = Console.ReadLine().Split();
-                    if(int.Parse(solvedBy[0]) == 1)
-                    {
-                        one++;
-                    }
-                    if (int.Parse(solvedBy[1]) == 1)
-                    {
-                        two++;
-                    }
-                }
-                int oneOne = one * colorOne;
-                int twoTwo = two * colorTwo;
-
-                int twoOne = two * colorOne;
-                int oneTwo = one * colorTwo;
-
-                result.Append(Math.Min((oneOne + twoTwo),(twoOne + oneTwo)) + "\n");
-            }
-            Console.WriteLine(result.ToString());
+            string isDivisible = intArray[n - 1] % 10 == 0 ? "Yes" : "No";
+            Console.WriteLine(isDivisible);
 
 
             //int numOfTestCases = int.Parse(Console.ReadLine());
