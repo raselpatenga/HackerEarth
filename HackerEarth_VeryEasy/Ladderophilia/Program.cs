f66f6eb4612d403f3e4ca0a72d4fc4587cd1c297
RASEL <raselpatenga@gmail.com>
2020-10-01 17:23:01 +0600

    Ladderophilia == Done

---
+++ HackerEarth_VeryEasy/Ladderophilia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladderophilia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("*   *");
+            Console.WriteLine("*   *");
+
+            for (int i = 0; i < input; i++)
+            {
+                Console.WriteLine("*****");
+                Console.WriteLine("*   *");
+                Console.WriteLine("*   *");
+            }
+            Console.ReadKey();
+        }
+    }
+}
